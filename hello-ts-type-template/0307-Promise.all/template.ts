d3bbdc268cc8a929f307e5f79a3a355fa549e8d6
Dannyzn <694518723@qq.com>
2022-03-07 20:43:40 +0800

    0307-pormise-all-理解

--- hello-ts-type-template/0307-Promise.all/template.ts
+++ hello-ts-type-template/0307-Promise.all/template.ts
@@ -3,3 +3,7 @@ type ExtractType<T> = T extends Promise<infer R> ? R : T;
 
 declare function PromiseAll<T extends any[]>(values: readonly[...T]): 
     Promise<{[P in keyof T]: ExtractType<T[P]>}> 
+
+
+    // 需要 看输入的是不是 promises 数组 或者说 可迭代的对象
+    // 返回值 是个promise 结果的数组
