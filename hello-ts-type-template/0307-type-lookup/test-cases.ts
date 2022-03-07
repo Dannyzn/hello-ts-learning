7fec278def6784aa893f6dce9564ad3060fb1607
Dannyzn <694518723@qq.com>
2022-03-07 20:37:51 +0800

    0307-type-lookup-try

---
+++ hello-ts-type-template/0307-type-lookup/test-cases.ts
@@ -0,0 +1,21 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type LookUp<U, T> = U extends { type: T } ? U : never
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]
