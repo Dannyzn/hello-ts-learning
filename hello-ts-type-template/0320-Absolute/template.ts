cf3f10f69cc18a1a82580b8a621478a5725dcdc4
Dannyzn <694518723@qq.com>
2022-03-20 21:46:35 +0800

    0320-try-answer

--- hello-ts-type-template/0320-Absolute/template.ts
+++ hello-ts-type-template/0320-Absolute/template.ts
@@ -1,3 +1,6 @@
 
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = 
+   `${T}` extends `-${infer U}` ? U : `${T}`
 
+
+// 核心解法是 把 T 变为字符串 然后检查字符串前面 是否有 '-' 我们就返回 之后的字符串, 否则转换为字符串的 T 
