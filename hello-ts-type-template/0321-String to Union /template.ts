9bdb68fefc2c475d283035620db0f81c43cb37ad
Dannyzn <694518723@qq.com>
2022-03-23 21:12:44 +0800

    0323-string-to-union-try

--- hello-ts-type-template/0321-String to Union /template.ts
+++ hello-ts-type-template/0321-String to Union /template.ts
@@ -1,1 +1,5 @@
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string> = T extends `${infer H}${infer R}` ? H | StringToUnion<R> : never
+
+// 利用 模版字符串
+
+// 将 字符串 分为  H 和 R 两部分,   [first, ...rest] 
