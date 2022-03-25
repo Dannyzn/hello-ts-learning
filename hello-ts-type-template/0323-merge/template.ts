a467430f62fbca8ba6cb8aafa93ea85bae60ddef
Dannyzn <694518723@qq.com>
2022-03-25 21:48:48 +0800

    0325-merge-not-understand

--- hello-ts-type-template/0323-merge/template.ts
+++ hello-ts-type-template/0323-merge/template.ts
@@ -1,1 +1,9 @@
-type Merge<F, S> = any;
+type Merge<F, S> = {
+    [Key in keyof F | keyof S]: Key extends keyof S ? S[Key] : Key extends keyof F ? F[Key] : never
+}
+
+// in 是否 属性在 F 或者 S 
+// 就是 借助于 extends
+
+// 不是很明白暂时
+//  哈哈
