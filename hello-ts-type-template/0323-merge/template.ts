790ef7a1f5aeade251a3c2bc81406fa534017077
Dannyzn <694518723@qq.com>
2022-03-26 21:38:53 +0800

    0326-merge-resolve

--- hello-ts-type-template/0323-merge/template.ts
+++ hello-ts-type-template/0323-merge/template.ts
@@ -1,3 +1,5 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
 type Merge<F, S> = {
     [Key in keyof F | keyof S]: Key extends keyof S ? S[Key] : Key extends keyof F ? F[Key] : never
 }
@@ -6,4 +8,23 @@ type Merge<F, S> = {
 // 就是 借助于 extends
 
 // 不是很明白暂时
-//  哈哈
+//  哈哈 因为是后面的属性 优先嘛？
+
+// 所以就是说 要 F|S  我们先去处理  S 中属性再去处理  F 中剩余的属性的值
+
+type Foo = {
+    a: number;
+    b: string;
+  };
+type Bar = {
+    b: number;
+    c: boolean;
+};
+  
+  type cases = [
+    Expect<Equal<Merge<Foo, Bar>, {
+      a: number;
+      b: number;
+      c: boolean;
+    }>>
+  ]
