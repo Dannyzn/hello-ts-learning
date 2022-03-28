107d8e784b6f46bb6e1e196fe3cf8d9138611a59
Dannyzn <694518723@qq.com>
2022-03-28 23:35:43 +0800

    feat: 0328-KebabCase

--- hello-ts-type-template/0328-kebaCase/template.ts
+++ hello-ts-type-template/0328-kebaCase/template.ts
@@ -1,2 +1,12 @@
+type LowerKebab<S> = S extends `${infer U}${infer R}`
+    ?  U extends LowerKebab<U>
+       ? `${U}${LowerKebab<R>}`
+       : `-${LowerKebab<U>}${LowerKebab<R>}`
+    : S;
 
-type KebabCase<S> = any;
+
+type KebabCase<S> = S extends '-'
+    ? S
+    : LowerKebab<S> extends `-${infer R}`
+        ? `${R}`
+        : LowerKebab<S>
